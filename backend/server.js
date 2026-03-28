841651b91fa464556090396c5357d66745c34513
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-03-28 19:59:34 +0530

    Initial commit - MERN project setup

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/",(req, res)=>{
-    res.status(200).json({ msg: "success from backend 123456" });
+    res.status(200).json({ msg: "success from backend 12345678" });
 });
 
 app.listen(3000,() => console.log ("server is running on port 3000"));
