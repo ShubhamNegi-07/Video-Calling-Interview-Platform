ba544667ac05a0ff35c0178629751e8c469de13a
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-03-30 21:33:02 +0530

    Initial commit - MERN project setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { ENV } from "./lib/env.js";
 
 const app = express();
 
-console.log(process.env.PORT);
-console.log(process.env.DB_URL);
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
 
 app.get("/",(req, res)=>{
     res.status(200).json({ msg: "success from backend 12345678" });
