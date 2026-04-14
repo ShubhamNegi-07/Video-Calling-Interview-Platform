8cea1d8b68a963567a2554011526855f0c67f7f1
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-04-14 21:07:49 +0530

    Initial commit - MERN project setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,16 @@
 import express from "express";
+import path from"patth"
+
 import { ENV } from "./lib/env.js";
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+const __dirname = path.resolve()
 
 app.get("/health",(req, res)=>{
     res.status(200).json({ msg: "api is up and running" });
 });
+
+//make our app ready for deployment 
 
 app.listen(ENV.PORT,() => console.log ("server is running on port:",ENV.PORT));
