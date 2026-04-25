f1f2fc5cc45fdbaaf42c68c79a7eca173a565a74
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-04-25 23:21:43 +0530

    feat: setup backend middleware and inngest configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import path from"path"
+import cors from"cors"
 
 import { ENV } from "./lib/env.js";
 
 const app = express();
 
 const __dirname = path.resolve()
+
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.get("/health",(req, res)=>{
     res.status(200).json({ msg: "api is up and running" });
