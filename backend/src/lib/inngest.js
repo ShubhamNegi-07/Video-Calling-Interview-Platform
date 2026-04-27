a14f31cc3a717ac13aefed8a0ff4821e7941f709
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-04-27 22:47:55 +0530

    Initial commit: Inngest setup and backend structure

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -15,9 +15,23 @@ const syncUser = inngest.createFunction(
 
     const newUser = {
       clerkId: id,
-      email: email_addresses[0]g(),
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      profileImage: image_url,
+    };
+
+    await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
       name: newUser.name,
       image: newUser.profileImage,
     });
   }
 );
+
+const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
