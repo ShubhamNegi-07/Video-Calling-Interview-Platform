43b77b3ea260f463211dca010e8c4860320e1419
ShubhamNegi-07 <shubhamsingh82735@gmail.com>
2026-05-01 10:17:18 +0530

    feat(problems): update ProblemsPage.jsx imports and problem data processing

---
+++ frontend/src/pages/ProblemsPage.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+import Navbar from "../components/Navbar";
+
+import { PROBLEMS } from "../data/problems";
+import { ChevronRightIcon, Code2Icon } from "lucide-react";
+import { getDifficultyBadgeClass } from "../lib/utils";
+
+function ProblemsPage() {
+  const problems = Object.values(PROBLEMS);
+
+  const easyProblemsCount = problems.filter((p) => p.difficulty === "Easy").length;
+  const mediumProblemsCount = problems.filter((p) => p.difficulty === "Medium").length;
+  const hardProblemsCount = problems.filter((p) => p.difficulty === "Hard").length;
+
+  return (
+    <div className="min-h-screen bg-base-200">
+      <Navbar />
